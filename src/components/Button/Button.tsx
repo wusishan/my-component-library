f5a068cbd247851218e207a8c78941fe61f0ac0f
Todd Chen <toddchen@nd.com.cn>
2022-02-22 15:11:37 +0800

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export interface ButtonProps {
+  label: string
+}
+
+const Button = (props: ButtonProps) => {
+  return (
+    <button>
+      {props.label}
+    </button>
+  )
+}
+
+export default Button
